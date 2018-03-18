6ae57ab92e5b2249fc352a0132ff39b5bdcf8d1a
priprakul <37500061+priprakul@users.noreply.github.com>
2018-03-18 17:55:11 +0530

    first

--- android/kalkans/app/src/main/java/com/bitjini/kalkans/MainActivity.java
+++ android/kalkans/app/src/main/java/com/bitjini/kalkans/MainActivity.java
@@ -1,9 +1,10 @@
 package com.bitjini.kalkans;
 
-import android.support.v7.app.Activity;
+// android.support.v7.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
