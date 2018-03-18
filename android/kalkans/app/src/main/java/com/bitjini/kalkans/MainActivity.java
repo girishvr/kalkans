4106c442028cb4810885d1f0554c6c46cf91bc18
girishvr <girish.vin.r@gmail.com>
2018-03-18 15:51:37 +0530

    Project folders created

---
+++ android/kalkans/app/src/main/java/com/bitjini/kalkans/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bitjini.kalkans;
+
+import android.support.v7.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
