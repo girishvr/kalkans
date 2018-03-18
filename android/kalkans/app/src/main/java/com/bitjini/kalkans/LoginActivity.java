dcacc25f92b62f4fd8267e3e3eb9f9229855d4f5
vidya <vidyaapyati@gmail.com>
2018-03-18 17:40:34 +0530

    Login, SignUp, Safety tips activity added

--- android/kalkans/app/src/main/java/com/bitjini/kalkans/LoginActivity.java
+++ android/kalkans/app/src/main/java/com/bitjini/kalkans/LoginActivity.java
@@ -1,14 +1,13 @@
 package com.bitjini.kalkans;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.support.v7.app.Activity;
 
-public class MainActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
