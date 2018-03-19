f9847cb772de60c39a35e9e6dc3d9e9b93de2044
priyankapk <priyankapk@github.com>
2018-03-19 09:31:19 +0530

    splash code

---
+++ android/kalkans/app/src/main/java/com/example/hp/kalkans/R.java
@@ -0,0 +1,9 @@
+package com.example.hp.kalkans;
+
+/**
+ * Created by Hp on 3/19/2018.
+ */
+
+class R {
+    public static Object layout;
+}
---
+++ android/kalkans/app/src/main/java/com/example/hp/kalkans/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.hp.kalkans;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bitjini.kalkans.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_tIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this,SplashActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_tIME_OUT);
+    }
+}
