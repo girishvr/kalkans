de5a31d72f0b13bcddb4ad5c7446baf166998c7a
girishvr <girish.vin.r@gmail.com>
2018-03-26 01:21:49 +0530

    Deployed on heroke

--- server/disaster/departments/models.py
+++ server/disaster/departments/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters.html import HtmlFormatter
-from pygments import highlight
+# from pygments.lexers import get_all_lexers
+# from pygments.styles import get_all_styles
+# from pygments.lexers import get_lexer_by_name
+# from pygments.formatters.html import HtmlFormatter
+# from pygments import highlight
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
+# LEXERS = [item for item in get_all_lexers() if item[1]]
 
 
 class department(models.Model):
--- server/disaster/persona/models.py
+++ server/disaster/persona/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
+# from pygments.lexers import get_all_lexers
+# from pygments.styles import get_all_styles
 
 
 
