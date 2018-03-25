c1570ef378345476a9b0ffe5b9416528478bfc17
girishvr <girish.vin.r@gmail.com>
2018-03-26 00:54:39 +0530

    lexers

--- departments/models.py
+++ departments/models.py
@@ -2,8 +2,8 @@ from django.db import models
 # from pygments.lexers import get_all_lexers
 # from pygments.styles import get_all_styles
 # from pygments.lexers import get_lexer_by_name
-from pygments.formatters.html import HtmlFormatter
-from pygments import highlight
+# from pygments.formatters.html import HtmlFormatter
+# from pygments import highlight
 
 # LEXERS = [item for item in get_all_lexers() if item[1]]
 
