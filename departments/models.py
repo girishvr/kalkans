bee9586dea93c7338ea54e1e486099f60f60bb20
girishvr <girish.vin.r@gmail.com>
2018-03-28 13:35:10 +0530

    Changes from prasad

--- departments/models.py
+++ departments/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-# from pygments.lexers import get_all_lexers
-# from pygments.styles import get_all_styles
-# from pygments.lexers import get_lexer_by_name
-# from pygments.formatters.html import HtmlFormatter
-# from pygments import highlight
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
-# LEXERS = [item for item in get_all_lexers() if item[1]]
+
 
 
 class department(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    
     dept_id = models.IntegerField(blank=False, default='')
     dept_name = models.CharField(max_length=40)
     location = models.CharField(max_length=40,default=False)
--- persona/models.py
+++ persona/models.py
@@ -15,7 +15,7 @@ class person(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
 
-    uid  = models.AutoField(unique=True, blank=False, default='', primary_key=True)
+    uid  = models.AutoField(primary_key=True)
     uname     = models.CharField(max_length=40, unique=False, blank=False, default='')
     pwd    = models.CharField(max_length=40, unique=False, blank=False, default='')
     utype    = models.CharField(max_length=20, unique=False, blank=False, default='User',choices=TYPES)
