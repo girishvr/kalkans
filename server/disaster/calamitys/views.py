97eb572740acda8d2968e7313c6562dec8506643
prasad <chachadiprasad@gmail.com>
2018-03-30 15:43:19 +0530

    Unique = True in departments table

--- server/disaster/calamitys/views.py
+++ server/disaster/calamitys/views.py
@@ -11,3 +11,23 @@ class calamityList(generics.ListCreateAPIView):
 class calamityDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = calamity.objects.all()
     serializer_class = calamitySerializer
+
+
+from math import sin, cos, sqrt, atan2
+
+R = 6373.0
+
+lat1 = department.objects.filter(lat)
+lon1 = department.objects.filter(lat)
+lat2 = 52.406374
+lon2 = 16.9251681
+
+dlon = lon2 - lon1
+dlat = lat2 - lat1
+a = (sin(dlat/2))**2 + cos(lat1) * cos(lat2) * (sin(dlon/2))**2
+c = 2 * atan2(sqrt(a), sqrt(1-a))
+distance = R * c
+
+print "Result", distance
+print "Should be", 278.546
+
--- server/disaster/departments/models.py
+++ server/disaster/departments/models.py
@@ -11,12 +11,12 @@ from pygments import highlight
 class department(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     
-    dept_id = models.IntegerField(blank=False, default='')
-    dept_name = models.CharField(max_length=40)
-    location = models.CharField(max_length=40,default=False)
-    team_id = models.CharField(max_length=40)
-    uname = models.CharField(max_length=40,default=False)
-    dept_pwd = models.CharField(max_length=40,default=False)
+    dept_id = models.AutoField(primary_key=True)
+    dept_name = models.CharField(unique=False, max_length=40)
+    location = models.CharField(unique=False,max_length=40,default=False)
+    team_id = models.CharField(unique=False,max_length=40)
+    uname = models.CharField(unique=True,max_length=40,default=False)
+    dept_pwd = models.CharField(unique=False,max_length=40,default=False)
     
 
     class Meta:
