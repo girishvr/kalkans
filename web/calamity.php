04dafc16833e690581bbf323ec556bed0c134287
girishvr <girish.vin.r@gmail.com>
2018-03-31 12:05:11 +0530

    Sound test Success

--- web/calamity.php
+++ web/calamity.php
@@ -6,11 +6,11 @@ session_cache_limiter("private_no_expire");
 ?>
 <head><title> calamity details</title>
    <!--<link rel="stylesheet" type="text/css" href="login.css">-->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" /> 
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" /> 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   
   <meta charset="utf-8">
-
+  <meta http-equiv="refresh" content="5"/>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
